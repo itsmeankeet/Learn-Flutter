ece682a963dc3f110b4b5ff357a66ad110fe8305
itsmeankeet <adheekariankit007@gmail.com>
2024-05-11 21:50:30 +0545

    Classes in dart added

---
+++ day2/classInDart.dart
@@ -0,0 +1,8 @@
+void main() {}
+
+class Person {
+  // properties and method include
+  String?
+      name; //name chai suru ma null define gareko ho yo bhneko suru ma value narakhi paxi rakhney bhneraw ho
+  int? age; // same as above
+}
