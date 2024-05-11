cfd43c680e8ab2cd721a24fc36045193180fa5fe
itsmeankeet <adheekariankit007@gmail.com>
2024-05-11 22:18:29 +0545

    Condition in Dart is Completed

--- day2/condition.dart
+++ day2/condition.dart
@@ -1,10 +1,21 @@
+import 'dart:ffi';
+
 void main() {
-  int age = 0;
+  int age = 15;
   if (age > 18) {
     print("You are a voter");
   } else if (age > 15 && age < 18) {
     print("Wait for few years to be voter");
   } else {
     print("Your are not a voter");
+  }
+
+  switch (age) {
+    case 15:
+      print("Your age is 15");
+      break;
+    case 18:
+      print("You can be voter");
+      break;
   }
 }
