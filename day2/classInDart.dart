5228ec7a23f6f85e0b92e47000c993e86b4ab0d6
itsmeankeet <adheekariankit007@gmail.com>
2024-05-11 22:02:05 +0545

    Classes In Dart

--- day2/classInDart.dart
+++ day2/classInDart.dart
@@ -1,8 +1,23 @@
-void main() {}
+void main() {
+  Person person = new Person();
+  person.name = "Cr7HoraaA";
+  person.age = 26;
+  person.display();
+
+  //person 1
+  Person person2 = new Person();
+  person2.name = "SekriHoraa";
+  person2.age = 23;
+  person2.display();
+}
 
 class Person {
-  // properties and method include
-  String?
-      name; //name chai suru ma null define gareko ho yo bhneko suru ma value narakhi paxi rakhney bhneraw ho
-  int? age; // same as above
+  String? name;
+  int? age;
+
+  //function to display
+  display() {
+    print(name);
+    print(age);
+  }
 }
